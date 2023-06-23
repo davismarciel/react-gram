617f32508af6701da313f74d756ad78e6d6a77ff
Davi Sá <davisdevi05@gmail.com>
2023-06-23 09:55:46 -0300

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,6 @@
 export const url = 'http://localhost:5000/api';
 export const uploads = 'http://localhost:5000/uploads';
 
-// export const url = 'https://tame-rose-pigeon-kit.cyclic.app/api';
-// export const uploads = 'https://tame-rose-pigeon-kit.cyclic.app/uploads';
 // Data =>
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
